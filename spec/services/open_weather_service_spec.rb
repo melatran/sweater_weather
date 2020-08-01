9cf07d16e7648d9ad4d89ea68d07bc2dd5a13fd6
Melanie Tran <melaniexx02@gmail.com>
2020-07-31 22:33:02 -0700

    Update test to pass travis

--- spec/services/open_weather_service_spec.rb
+++ spec/services/open_weather_service_spec.rb
@@ -4,13 +4,11 @@ describe OpenWeatherService do
   it ".get_forecast_data" do
     service = OpenWeatherService.new
     results = service.get_forecast_data(33.834492, -117.915638)
-
-    # expect(results[:current]).to_not be_empty
-    # expect(results[:hourly]).to_not be_empty
-    # expect(results[:daily]).to_not be_empty
-
-    #8 results displayed in wireframe
+  
+    expect(results[:current]).to be_a(Hash)
+    expect(results[:hourly]).to be_an(Array)
     expect(results[:daily]).to be_an(Array)
-    # expect(results[:daily].length).to eq(8)
+    #8 results from wireframe
+    expect(results[:daily].length).to eq(8)
   end
 end
