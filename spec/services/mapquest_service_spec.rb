57df04ac4c231f45d6cf999fdd97678eabfd88cf
Melanie Tran <melaniexx02@gmail.com>
2020-07-31 19:27:02 -0700

    Remove comments

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -2,18 +2,11 @@ require 'rails_helper'
 
 describe MapQuestService do
   it ".get_coordinates" do
-    #get lat and long for city
     service = MapQuestService.new
-    map_info = service.get_coordinates("Anaheim, CA") #coordinates for disneyland
+    map_info = service.get_coordinates("Anaheim, CA")
 
     expect(map_info[:results]).to be_an(Array)
     expect(map_info[:results][0][:locations][0][:latLng][:lat]).to eq(33.834492) #lat
     expect(map_info[:results][0][:locations][0][:latLng][:lng]).to eq(-117.915638) #long
   end
 end
-
-
-#Needs to pull out the city and state from the GET request and send it to MapQuest’s Geocoding API to retrieve the lat and long for the city (this can be its own story). Use of the MapQuest’s Geocoding API is a hard requirement.
-
-#results[:results][0][:locations][0][:latLng][:lng]]
-#results[:results][0][:locations][0][:latLng][:lng]
