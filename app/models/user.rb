157f8c67270171bf1d5a86b1b6ca9e87037f6e6d
Melanie Tran <melaniexx02@gmail.com>
2020-08-02 06:52:26 -0700

    Add validation for api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
+  validates :api_key, uniqueness: true, presence: true
   validates_presence_of :password_digest, require: true
 end
