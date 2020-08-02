01fd77799e3f479f6e0c87ad44db517c52fe5c35
Melanie Tran <melaniexx02@gmail.com>
2020-08-02 08:38:29 -0700

    Fix indentation

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-  location = MapQuestService.new.get_coordinates(params[:location])
-  location_info = Location.new(location)
-  lat = location_info.lat
-  lon = location_info.lon
-  weather_data = OpenWeatherService.new.get_forecast_data(lat, lon)
-  city_forecast = Forecast.new(location_info, weather_data)
+    location = MapQuestService.new.get_coordinates(params[:location])
+    location_info = Location.new(location)
+    lat = location_info.lat
+    lon = location_info.lon
+    weather_data = OpenWeatherService.new.get_forecast_data(lat, lon)
+    city_forecast = Forecast.new(location_info, weather_data)
 
-  render json: ForecastSerializer.new(city_forecast)
+    render json: ForecastSerializer.new(city_forecast)
   end
 end
