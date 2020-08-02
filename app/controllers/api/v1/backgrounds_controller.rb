127ae4d37515ab3ef0706549f3181da9ee93772c
Melanie Tran <melaniexx02@gmail.com>
2020-08-01 23:02:04 -0700

    Create background endpoint

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
+    image_data = UnsplashService.new.get_background_url(params[:location])
+    photo = Photo.new(image_data)
+
+    render json: BackgroundsSerializer.new(photo)
   end
 end
