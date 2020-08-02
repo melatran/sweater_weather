0ba161f6347bc9e19e86044e371538fe5b8b3e44
Melanie Tran <melaniexx02@gmail.com>
2020-08-02 07:02:19 -0700

    Update user model with secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :api_key, uniqueness: true, presence: true
-  validates_presence_of :password_digest, require: true
+  validates :password_digest, presence: true
+
+  has_secure_password
+
 end
