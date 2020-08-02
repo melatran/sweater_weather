e030c33d07a7fa0825e23427cf852336bbd2d79b
Melanie Tran <melaniexx02@gmail.com>
2020-08-02 08:08:04 -0700

    Add create method for users route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.create(user_params)
+
+    if user.save
+      render json: UserSerializer.new(user)
+    else
+      render :json => { :errors => user.errors.full_messages }
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
