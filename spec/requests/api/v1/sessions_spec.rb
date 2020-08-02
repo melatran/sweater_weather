2d1bc4ebc8893f0741f4829d7764eb43f9bf7817
Melanie Tran <melaniexx02@gmail.com>
2020-08-02 10:46:56 -0700

    Add sad path testing for sessions

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -21,5 +21,36 @@ describe 'Sessions endpoints' do
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
+    expect(json[:data][:attributes].keys).to eq([:id, :email, :api_key])
+    expect(json[:data][:attributes][:email]).to eq('email@gmail.com')
+    expect(json[:data][:attributes][:api_key]).to_not be_nil
+  end
+
+  it "cant find user if they didn't register" do
+    session_params = {
+      'email': '1234@gmail.com',
+      'password': 'password'
+    }
+
+    post '/api/v1/sessions', params: session_params
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(json[:email]).to eq(["Invalid email address"])
+  end
+
+  it "cant login if password incorrect" do
+    session_params = {
+      'email': 'email@gmail.com',
+      'password': '1234'
+    }
+
+    post '/api/v1/sessions', params: session_params
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(json[:password]).to eq(['Incorrect password'])
   end
 end
