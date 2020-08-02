ea588a91f94372440e6c7c7854a722ccb0943a01
Melanie Tran <melaniexx02@gmail.com>
2020-08-01 21:44:08 -0700

    Make test more robust

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -9,6 +9,16 @@ describe "Forecast API" do
     expect(response).to be_successful
 
     forecast = JSON.parse(response.body, symbolize_names: true)
-    
+    expected_keys = [:id,
+              :timezone,
+              :current_forecast,
+              :hourly_forecast,
+              :daily_forecast]
+
+    expect(forecast[:data][:attributes].keys).to eq(expected_keys)
+    expect(forecast).to be_a(Hash)
+    expect(forecast[:data][:attributes][:current_forecast][:temp]).to eq(70.59)
+    expect(forecast[:data][:attributes][:hourly_forecast].length).to eq(48)
+    expect(forecast[:data][:attributes][:daily_forecast].length).to eq(8)
   end
 end
