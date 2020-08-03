f500d6d90d1bbae96fc201501350aab2a678b03e
Melanie Tran <melaniexx02@gmail.com>
2020-08-03 10:03:42 -0700

    Create get_directions method

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -11,4 +11,12 @@ describe MapQuestService do
     expect(map_info[:results][0][:locations][0][:latLng][:lat]).to eq(33.834492) #lat
     expect(map_info[:results][0][:locations][0][:latLng][:lng]).to eq(-117.915638) #long
   end
+
+  it ".get_directions" do
+    service = MapQuestService.new
+    directions = service.get_directions("Denver, Co", "Boulder, CO")
+
+    expect(directions).to be_a(Hash)
+    expect(directions[:route][:legs][0][:maneuvers]).to_not be_nil
+  end
 end
