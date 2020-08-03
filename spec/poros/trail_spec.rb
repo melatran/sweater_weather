77a16fc4675a6c8cb1e9729e655c49e2bba30429
Melanie Tran <melaniexx02@gmail.com>
2020-08-03 08:25:04 -0700

    Rename spec

--- spec/poros/trail_spec.rb
+++ spec/poros/trail_spec.rb
@@ -4,6 +4,14 @@ describe Trail do
   before :each do
     json_response = File.read('./spec/fixtures/hiking_response.json')
     trails_info = JSON.parse(json_response, symbolize_names: true)
-    @trails = Trail.new(location_info)
+    @trails = Trail.new(trails_info)
+  end
+
+  it "exists" do
+    expect(@trails).to be_a(Trail)
+  end
+
+  it "can return attributes about trails" do
+  
   end
 end
