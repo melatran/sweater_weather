6dd7b08477858a30c6d68aac32ea9c3af8be56cb
Melanie Tran <melaniexx02@gmail.com>
2020-08-03 08:14:22 -0700

    Fix errors on test

--- spec/services/hiking_service_spec.rb
+++ spec/services/hiking_service_spec.rb
@@ -4,7 +4,10 @@ describe HikingService do
   it ".get_trails" do
     WebMock.allow_net_connect!
 
-    service = MapQuestService.new
-    trail_info = service.get_coordinates(40.0274, -105.2519)
+    service = HikingService.new
+    trail_info = service.get_trails(40.0274, -105.2519)
+
+    expect(trail_info).to be_a(Hash)
+    expect(trail_info[:trails]).to_not be_nil
   end
 end
