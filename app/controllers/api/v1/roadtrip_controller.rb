198b3cf967f5cf00264b06cd02edfeef96edf449
Melanie Tran <melaniexx02@gmail.com>
2020-08-03 14:28:44 -0700

    Create roadtrip endpoint

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RoadtripController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
     if user
-      
-    #params origin, desination, api_key
+      render json: Roadtrip.new(params[:origin], params[:destination]).format_roadtrip
+    end
   end
 end
