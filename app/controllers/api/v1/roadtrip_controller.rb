d2108c582f975aa0a66dd3f42f79fd570d995115
Melanie Tran <melaniexx02@gmail.com>
2020-08-03 15:12:47 -0700

    Move authenticate user to a private method

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::RoadtripController < ApplicationController
   def create
-    user = User.find_by(api_key: params[:api_key])
-    if user
+    if authenticated_user?
       roadtrip = Roadtrip.new(params[:origin], params[:destination]).format_roadtrip
       render json: {status: 200, roadtrip: roadtrip}, status: 200
     end
+  end
+
+  private
+
+  def authenticated_user?
+    User.find_by(api_key: params[:api_key])
   end
 end
