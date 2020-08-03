d892fd4f999e17ef5763d3428979931ae38554cb
Melanie Tran <melaniexx02@gmail.com>
2020-08-03 08:30:24 -0700

    Create get trails info method

--- app/poros/trail.rb
+++ app/poros/trail.rb
@@ -1,2 +1,13 @@
 class Trail
+  attr_reader :trail_info
+
+  def initialize(data)
+    @trail_info = get_trails_info(data)
+  end
+
+  def get_trails_info(data)
+    data[:trails].map do |data_info|
+      data_info.slice(:name, :summary, :difficulty, :location)
+    end
+  end
 end
