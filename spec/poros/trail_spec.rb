d70aa5afb4b33c47fc9036a897638fddecbf42bd
Melanie Tran <melaniexx02@gmail.com>
2020-08-03 08:34:26 -0700

    Update test with the correct information

--- spec/poros/trail_spec.rb
+++ spec/poros/trail_spec.rb
@@ -15,7 +15,7 @@ describe Trail do
     expect(@trails.trail_info.length).to eq(10)
     expect(@trails.trail_info[0][:name]).to eq("Monarch Lake to Crater Lake")
     expect(@trails.trail_info[0][:summary]).to eq("Gorgeous scenery with waterfalls and wildflowers ending at a breathtaking cirque and glacier lake.")
-    expect(@trails.trail_info[0][:difficulty]).to eq("black")
+    expect(@trails.trail_info[0][:difficulty]).to eq("blueBlack")
     expect(@trails.trail_info[0][:location]).to eq("Granby, Colorado")
   end
 end
